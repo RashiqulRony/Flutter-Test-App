377fb539b60671c0cc69a27b3326bbad09dd4f63
Rashiqul Islam <rashiqul@smeinformatics.com>
2022-06-23 18:23:41 +0600

    Text input working

--- lib/screen/textfieldwidget.dart
+++ lib/screen/textfieldwidget.dart
@@ -8,7 +8,19 @@ class TextFieldWidget extends StatefulWidget {
 }
 
 class _TextFieldWidgetState extends State<TextFieldWidget> {
+
+  final TextEditingController controller = TextEditingController();
+
   @override
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addListener(() {
+      setState(() {});
+    });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -17,7 +29,96 @@ class _TextFieldWidgetState extends State<TextFieldWidget> {
         centerTitle: true,
       ),
 
-      body: ListView(children: []),
+      body: ListView(
+        padding: EdgeInsets.all(30),
+        children: [
+          TextField(
+            textInputAction: TextInputAction.next,
+            keyboardType: TextInputType.text,
+            controller: controller,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: "Name",
+              hintText: "Mr. Alex",
+              prefixIcon: Icon(Icons.people),
+              suffixIcon: controller.text.isEmpty ? Container(width: 0) : IconButton(
+                onPressed: (){
+                  controller.clear();
+                }, 
+                icon: Icon(Icons.clear)
+              )
+            ),
+        ),
+        // SizedBox(height: 25),
+        // TextField(
+        //   controller: controller,
+        //     decoration: InputDecoration(
+        //       border: OutlineInputBorder(),
+        //       labelText: "Email",
+        //       hintText: "test@test.com",
+        //       prefixIcon: Icon(Icons.mail),
+        //       suffixIcon: controller.text.isEmpty ? Container(width: 0) : IconButton(onPressed: (){
+        //         controller.clear();
+        //       }, icon: Icon(Icons.clear))
+        //     ),
+        // ),
+
+        // SizedBox(height: 25),
+        // TextField(
+        //   controller: controller,
+        //     decoration: InputDecoration(
+        //       border: OutlineInputBorder(),
+        //       labelText: "Phone",
+        //       hintText: "0100000000",
+        //       prefixIcon: Icon(Icons.phone),
+        //       suffixIcon: controller.text.isEmpty ? Container(width: 0) : IconButton(onPressed: (){
+        //         controller.clear();
+        //       }, icon: Icon(Icons.clear))
+        //     ),
+        // ),
+
+        // SizedBox(height: 25),
+        // TextField(
+        //   controller: controller,
+        //     decoration: InputDecoration(
+        //       border: OutlineInputBorder(),
+        //       labelText: "Password",
+        //       prefixIcon: Icon(Icons.password),
+        //       suffixIcon: controller.text.isEmpty ? Container(width: 0) : IconButton(onPressed: (){
+        //         controller.clear();
+        //       }, icon: Icon(Icons.remove_red_eye))
+        //     ),
+        // ),
+
+        // SizedBox(height: 25),
+        // TextField(
+        //   controller: controller,
+        //     decoration: InputDecoration(
+        //       border: OutlineInputBorder(),
+        //       labelText: "Confirm Password",
+        //       prefixIcon: Icon(Icons.password),
+        //       suffixIcon: controller.text.isEmpty ? Container(width: 0) : IconButton(onPressed: (){
+        //         controller.clear();
+        //       }, icon: Icon(Icons.remove_red_eye))
+        //     ),
+        // ),
+
+        //  SizedBox(height: 25),
+
+        //     OutlinedButton(
+        //       style: OutlinedButton.styleFrom(
+        //         minimumSize: Size(100, 50),
+        //         primary: Colors.green,
+        //         side: BorderSide(width: 2, color: Colors.green)
+        //       ),
+            
+        //       onPressed: () {
+        //         print("Outlined Button Pressed");
+        //       }, 
+        //       child: Text("Submit"),
+            
+        //     ),
+      ]),
     );
   }
 }
